1f7188732ace1f46c08e2f922a6187a4650fa6f3
jacky_lau <jackylau988@gmail.com>
2019-04-21 12:32:30 +0800

    Tab icon changed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,29 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireStorageModule } from 'angularfire2/storage';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+var config = {
+  apiKey: "AIzaSyB928A-FuSKpYAYs5iBfXBt4TZxUn2tq5w",
+  authDomain: "poca-d58dd.firebaseapp.com",
+  databaseURL: "https://poca-d58dd.firebaseio.com",
+  projectId: "poca-d58dd",
+  storageBucket: "poca-d58dd.appspot.com",
+  messagingSenderId: "190095561574"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    AngularFireModule,
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(config)],
   providers: [
     StatusBar,
     SplashScreen,
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+export class Tab1Page {
+  propertyList$ : Observable<any[]>;
+  propertyList = new Array<any>();
+  firebaseRef = 'propertyList';
+
+  constructor(
+    public afd: AngularFireDatabase,
+  ) {
+    console.log("wt?");
+    this.getDataFromFireBase();
+  }
+
+  getDataFromFireBase(){
+    this.propertyList$ = this.afd.list(this.firebaseRef).valueChanges() as Observable<any[]>;
+    this.propertyList$.subscribe(
+        item => {
+          this.propertyList = item;
+          console.log(this.propertyList);
+        });
+  }
+
+}
