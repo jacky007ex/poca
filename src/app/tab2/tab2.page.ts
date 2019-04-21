5568cb4a9d79125f0d12671602c0a13eaa2b3034
awesomemind <47248691+awesomemind@users.noreply.github.com>
2019-04-22 05:58:03 +0800

    add "chat" page

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+
+export class Tab2Page {
+  pageHeader: string = "My groups";
+  groups: string[]; 
+
+  constructor(public navCtrl: NavController, public router: Router) {
+    this.groups = [
+      'group 1',
+      'group 2',
+      'group 3'
+    ];
+  }
+
+  openGroup(group) {
+    console.log('group clicked:', group);
+    // this.navCtrl.navigateForward('/chat');
+    this.router.navigate(['/chat', {group: group}]);
+  }
+}
